26cce9d5d9d54d84409791edd55572bf9009aa7a
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-03-20 13:11:01 +0300

    update requirements.txt

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Build the project
+# Install pip if missing
+echo "Checking if pip is installed..."
+python3 -m ensurepip --default-pip
+
+# Install project dependencies
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
+# Apply migrations
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear

804d66a5776f339977d0eac883662f49a20a32b2
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-03-21 15:04:05 +0300

    fixed drop off time

--- planner/services/eld_service.py
+++ planner/services/eld_service.py
@@ -14,7 +14,6 @@ class ELDService:
         
         # Start with current time as the beginning of the trip
         current_time = datetime.now()
-        
         # Initialize cycle
         remaining_drive_time = self.max_driving_hours - current_cycle_used
         remaining_duty_time = self.max_on_duty_hours - current_cycle_used
@@ -24,19 +23,48 @@ class ELDService:
         current_log = self._initialize_log_sheet(current_time.date())
         
         # Initial on-duty status the driver is going to pick up the load
-
-        current_status = "ON"
-        current_log['events'].append({
-            "time": current_time.strftime("%H:%M"),
-            "status": current_status,
-            "location": trip_details['to_pickup']['features'][0]['geometry']['coordinates'][0] if 'to_pickup' in trip_details else "Unknown"
-        })
-        
+    
+        # Check if there is to pick up coordinate so that you drive and head to pick up location
+        if len(trip_details['to_pickup']['features'][0]['geometry']['coordinates']) > 0:
+            current_status = "D"
+            current_log['events'].append({
+                "time": current_time.strftime("%H:%M"),
+                "status": current_status,
+                "location": trip_details['to_pickup']['features'][0]['geometry']['coordinates'][0] if 'to_pickup' in trip_details else "Unknown"
+            })
+            
+        # Get both drop off location and pick up location duration taken
+        pickup_location_duration = (trip_details['to_pickup']['features'][0]['properties']['summary']['duration'] / 3600) if 'to_pickup' in trip_details else 0
+        dropoff_location_duration = (trip_details['pickup_to_dropoff']['features'][0]['properties']['summary']['duration'] / 3600) if 'pickup_to_dropoff' in trip_details else 0
+                
         # Simulate the trip hour by hour
         hours_simulated = 0
+        pickup_location_hours = 0
         while hours_simulated < total_duration:
+            # CHeck if pickup location has been reached
+            if hours_simulated < pickup_location_duration:
+                updated_time = current_time + timedelta(hours=pickup_location_duration)
+                current_time += timedelta(hours=pickup_location_duration)
+                # Add pickup
+                current_status = "ON"
+                current_log['events'].append({
+                    "time": updated_time.strftime("%H:%M"),
+                    "status": current_status,
+                    "location": trip_details['pickup_to_dropoff']['features'][0]['geometry']['coordinates'][0] if 'pickup_to_dropoff' in trip_details else "Unknown",
+                    "remarks": "Pickup location"
+                })
+                
+                current_time += timedelta(hours=1)
+                
+                current_status = "D"
+                current_log['events'].append({
+                    "time": current_time.strftime("%H:%M"),
+                    "status": current_status,
+                    "location": trip_details['pickup_to_dropoff']['features'][0]['geometry']['coordinates'][0] if 'pickup_to_dropoff' in trip_details else "Unknown",
+                })
+                
+            hours_simulated += 1               
             current_time += timedelta(hours=1)
-            hours_simulated += 1
             
             # Check if we need to start a new day
             if current_time.date() != datetime.strptime(current_log['date'], "%Y-%m-%d").date():
@@ -99,7 +127,7 @@ class ELDService:
                 })
             
             # Check for fuel stops
-            if trip_details['stops']['fuel_stops']["number_of_fuel_stops"] > 0 and hours_simulated % (total_duration / (trip_details['stops']['fuel_stops'] + 1)) < 1:
+            if trip_details['stops']['fuel_stops']["number_of_fuel_stops"] > 0 and hours_simulated % (total_duration / (trip_details['stops']['fuel_stops']["number_of_fuel_stops"] + 1)) < 1:
                 current_status = "ON"
                 current_log['events'].append({
                     "time": current_time.strftime("%H:%M"),
@@ -118,24 +146,6 @@ class ELDService:
                     "status": current_status,
                     "location": "Fuel stop"
                 })
-        
-        # Add pickup and dropoff
-        current_status = "ON"
-        current_log['events'].append({
-            "time": current_time.strftime("%H:%M"),
-            "status": current_status,
-            "location": trip_details['pickup_to_dropoff']['features'][0]['geometry']['coordinates'][0] if 'pickup_to_dropoff' in trip_details else "Unknown",
-            "remarks": "Pickup location"
-        })
-        
-        current_time += timedelta(hours=1)  # 1 hour for pickup
-        
-        current_status = "D"
-        current_log['events'].append({
-            "time": current_time.strftime("%H:%M"),
-            "status": current_status,
-            "location": trip_details['pickup_to_dropoff']['features'][0]['geometry']['coordinates'][0] if 'pickup_to_dropoff' in trip_details else "Unknown",
-        })
         
         # Add the final dropoff
         current_time = current_time + timedelta(hours=total_duration-hours_simulated)
